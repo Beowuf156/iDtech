87d92e7c32bb2263ca37a39fa14a877128fb34b8
beowulf <pcmasterbewould@outlook.com>
2025-07-18 10:39:41 -0700

    main update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ parser.add_argument("--labels", type=str, default="labels.txt", help="gives name
 
 opt = parser.parse_args()
 img = jetson_utils.loadImage(opt.filename)
-net = jetson_inference.imageNet(model=opt.model,labels=opt.labels)
+net = jetson_inference.imageNet(model=opt.model,labels=opt.labels, input_blob="input_0", output_blob="output_0")
 class_idx, confidence = net.Classify(img)
 class_desc = net.GetClassDesc(class_idx)
 print("image is recognized as "+ str(class_desc) +" (class #"+ str(class_idx) +") with " + str(confidence*100)+"% confidence")
